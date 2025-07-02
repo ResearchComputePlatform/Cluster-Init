da0b204530b0cfac95bc5867d0e58027ab030bf2
Steven Ossont <Steven.Ossont@Bizdata.co.uk>
2025-07-02 14:58:32 +1200

    firstpass

---
+++ projects/clusterdefault/specs/default/cluster-init/scripts/00_install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -x                         # tell sh to display commands before execution
+echo "Starting....."
+
+logFileName="/var/log/clusterdefault.log"
+touch $logFileName
+
+# Send all as a redirect , but then tee so we can see logs and output
+exec &> >(tee -a "$logFileName")
+
+set -eo pipefail
+read_os()
+{
+    os_release=$(cat /etc/os-release | grep "^ID\=" | cut -d'=' -f 2 | xargs)
+    os_maj_ver=$(cat /etc/os-release | grep "^VERSION_ID\=" | cut -d'=' -f 2 | xargs)
+    full_version=$(cat /etc/os-release | grep "^VERSION\=" | cut -d'=' -f 2 | xargs)
+}
+
+read_os
+case $os_release in
+    almalinux)
+        logger -s  "Not Implemented"
+        ;;
+    ubuntu)
+        
+        apt-get update
+        apt-get install -y moreutils
+        ;;
+    *)
+        logger -s "Untested OS $os_release $os_version"
+        exit 0
+        ;;
+esac
